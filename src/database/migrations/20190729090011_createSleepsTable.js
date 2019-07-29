274cc077729dcafcd7e3b28dd0f373e0cbfa2cac
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-29 15:11:03 +0100

    Format some files

--- src/database/migrations/20190729090011_createSleepsTable.js
+++ src/database/migrations/20190729090011_createSleepsTable.js
@@ -1,20 +1,20 @@
 exports.up = function migrate(knex) {
-	return knex.schema.createTable('sleeps', (tbl) => {
-		tbl.increments();
-		tbl
-			.integer('user_id')
-			.unsigned()
-			.notNullable()
-			.references('id')
-			.inTable('users')
-			.onUpdate('CASCADE')
+  return knex.schema.createTable('sleeps', (tbl) => {
+    tbl.increments();
+    tbl
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
       .onDelete('CASCADE');
     tbl.datetime('sleep_time').notNullable();
     tbl.datetime('wake_time');
-		tbl.float('score').unsigned();
-	});
+    tbl.float('score').unsigned();
+  });
 };
 
 exports.down = function rollback(knex) {
-	return knex.schema.dropTableIfExists('sleeps');
+  return knex.schema.dropTableIfExists('sleeps');
 };
