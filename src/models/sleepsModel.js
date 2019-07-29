27e4139cf4a5e4fdd8b0c3770eff3c9b670ab7ec
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-29 09:25:40 +0100

    Add sleeps model skeleton code

---
+++ src/models/sleepsModel.js
@@ -0,0 +1,34 @@
+const db = require('../database/dbConfig');
+
+async function insert(sleep) {
+    return null;
+}
+
+function getAll() {
+    return null;
+}
+
+function findById(id) {
+    return null;
+}
+
+function findByUser(id) {
+    return null;
+}
+
+async function update(id) {
+    return null;
+}
+
+async function remove(id) {
+    return null;
+}
+
+module.exports = {
+    insert,
+    getAll,
+    findById,
+    findByUser,
+    update,
+    remove
+}
