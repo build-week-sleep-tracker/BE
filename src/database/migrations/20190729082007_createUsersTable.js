b72ef5f7c0115948e6500882c127ba3789595d2e
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-29 08:59:59 +0100

    Add users migration

---
+++ src/database/migrations/20190729082007_createUsersTable.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
+        tbl.string('email', 128).unique().notNullable();
+        tbl.string('password').notNullable();
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users');
+};
