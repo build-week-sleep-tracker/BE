8e26f0ccbaa8a5636dcded94708c4fccabc6b399
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-29 13:40:27 +0100

    Finish sleep model tests

--- src/api/models/sleepsModel.test.js
+++ src/api/models/sleepsModel.test.js
@@ -1,4 +1,4 @@
-const db = require('../database/dbConfig');
+const db = require('../../database/dbConfig');
 const SleepsModel = require('./sleepsModel');
 
 beforeEach(() => {
@@ -7,12 +7,43 @@ beforeEach(() => {
     .then(() => db('users').truncate());
 });
 
+async function createTestUser(email = 'test') {
+  await db('users').insert({ email, password: '1234' });
+}
+
 describe('Adding and manipulation sleeps table', () => {
-  test('Can add sleep', async (done) => {
-    await db('users').insert({ email: 'test', password: '1234' });
-    await SleepsModel.insert({ user_id: 1, sleep_time: '2019-07-29T21:53:00' });
+  test('Can add sleep and returns sleep', async () => {
+    await createTestUser();
+    const sleep = await SleepsModel.insert({ user_id: 1, sleep_time: '2019-07-29T21:53:00' });
     const sleeps = await SleepsModel.getAll();
     expect(sleeps).toHaveLength(1);
-    done();
+    expect(sleep).toBeDefined();
   });
+
+  test('Can update sleep and returns sleep', async () => {
+    await createTestUser();
+    const sleep = await SleepsModel.insert({ user_id: 1, sleep_time: '2019-07-29T21:53:00' });
+    const sleep2 = await SleepsModel.update(sleep.id, { wake_time: '2019-07-30T7:53:00' });
+    expect(sleep).not.toEqual(sleep2);
+    expect(sleep2).toHaveProperty('wake_time', '2019-07-30T7:53:00');
+  });
+
+  test('Can delete sleep and returns old sleep', async () => {
+    await createTestUser();
+    const sleep = await SleepsModel.insert({ user_id: 1, sleep_time: '2019-07-29T21:53:00' });
+    const sleep2 = await SleepsModel.remove(sleep.id);
+    const sleeps = await SleepsModel.getAll();
+    expect(sleep).toEqual(sleep2);
+    expect(sleeps).toHaveLength(0);
+  });
+
+  test('Can get sleeps by user', async () => {
+    await createTestUser();
+    await createTestUser('test2');
+    await SleepsModel.insert({ user_id: 1, sleep_time: '2019-07-29T21:53:00' });
+    await SleepsModel.insert({ user_id: 2, sleep_time: '2019-07-29T21:53:00' });
+    await SleepsModel.insert({ user_id: 2, sleep_time: '2019-07-29T21:53:00' });
+    const sleeps = await SleepsModel.findByUser(2);
+    expect(sleeps).toHaveLength(2);
+  })
 });
