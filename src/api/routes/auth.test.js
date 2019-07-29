f14764535abd7cfb5554327369516e68acd9490f
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-29 14:13:50 +0100

    Write all auth tests

--- src/api/routes/auth.test.js
+++ src/api/routes/auth.test.js
@@ -14,4 +14,16 @@ describe('Register and login users', () => {
     const users = await UsersModel.getAll();
     expect(users).toHaveLength(1);
   });
+
+  test('Can login user and returns user', async () => {
+    await request(server)
+      .post('/api/register')
+      .send({ email: 'test', password: '1234' })
+      .expect(200);
+    const res = await request(server)
+      .post('/api/login')
+      .send({ email: 'test', password: '1234' })
+      .expect(200);
+    expect(res.body.user).toEqual({ id: 1, email: 'test' });
+  });
 });
