7bc5156781c3c7d66875eb2922c26c30393a578a
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-29 11:47:55 +0100

    Add cookie support

--- src/api/middleware/globalMiddleware.js
+++ src/api/middleware/globalMiddleware.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const dbConfig = require('../../database/dbConfig.js');
+const { cookieSecret } = require('../secrets');
 
 module.exports = (server) => {
   server.use(express.json());
   server.use(helmet());
   server.use(cors());
+  server.use(session({
+    name: 'sessionID',
+    secret: cookieSecret,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+      httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new KnexSessionStore({
+      knex: dbConfig, // configured instance of knex
+      tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+      sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+      createtable: true, // if the table does not exist, it will create it automatically
+      clearInterval: 1000 * 60 * 60,
+    }),
+  }));
 }
---
+++ src/api/secrets/index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+  cookieSecret: process.env.COOKIE_SECRET || 'IZoy4LYbCRyKtdBD5Sev7Nnw_3uydhYNqaelQOUX3-WNWCaJP9FZ2csiUo371BIXuInnrtG',
+};
