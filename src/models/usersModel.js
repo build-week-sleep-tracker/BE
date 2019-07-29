e1f9ad547b5acdfffaf87b94f656013814b4aab8
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-29 09:24:12 +0100

    Add users model skeleton code

---
+++ src/models/usersModel.js
@@ -0,0 +1,34 @@
+const db = require('../database/dbConfig');
+
+async function insert(user) {
+    return null;
+}
+
+function getAll() {
+    return null;
+}
+
+function findById(id) {
+    return null;
+}
+
+function findByUsername(id) {
+    return null;
+}
+
+async function update(id) {
+    return null;
+}
+
+async function remove(id) {
+    return null;
+}
+
+module.exports = {
+    insert,
+    getAll,
+    findById,
+    findByUsername,
+    update,
+    remove
+}
